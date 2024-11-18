7d29c2b617ceac07ad448c90ee901ea800ae3796
yokohama <yuhei.yokohama@gmail.com>
2024-11-18 11:58:44 +0900

    init

--- front/src/app/shuting/page.tsx
+++ front/src/app/shuting/page.tsx
@@ -6,18 +6,40 @@ export default function Page() {
   return(
     <>
       <div>
-        <Link href={`/shuting/1`} key="1">
-          <div
-            className="bg-red-50 mb-4 p-2 h-16 rounded hover:bg-red-100 transition-all duration-200 ease-in-out">
-            <p>Level1</p>
-          </div>
-        </Link>
-        <Link href={`/shuting/2`} key="2">
-          <div
-            className="bg-red-50 mb-4 p-2 h-16 rounded hover:bg-red-100 transition-all duration-200 ease-in-out">
-            <p>Level2</p>
-          </div>
-        </Link>
+
+        <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-1">
+          <Link href={`/shuting/1`} key="1">
+            <div className="bg-white shadow-md rounded-lg flex items-center h-32 transition-transform transform hover:scale-105 hover:bg-gray-100">
+              <img
+                src="/images/level1.png"
+                alt="Level1 illustration"
+                className="h-full w-auto object-cover mr-4 rounded-tl-lg rounded-bl-lg"
+              />
+              <div>
+                <h3 className="text-3xl font-bold">Level1</h3>
+                <p className="text-lg text-gray-500">
+                  初心者向けのチャレンジ！
+                </p>
+              </div>
+            </div>
+          </Link>
+
+          <Link href={`/shuting/2`} key="2">
+            <div className="bg-white shadow-md rounded-lg flex items-center h-32 transition-transform transform hover:scale-105 hover:bg-gray-100">
+              <img
+                src="/images/level2.png"
+                alt="Level2 illustration"
+                className="h-full w-auto object-cover mr-4 rounded-tl-lg rounded-bl-lg"
+              />
+              <div>
+                <h3 className="text-3xl font-bold">Level2</h3>
+                <p className="text-lg text-gray-500">
+                  初心者向けのチャレンジ！
+                </p>
+              </div>
+            </div>
+          </Link>
+        </div>
       </div>
     </>
   );
