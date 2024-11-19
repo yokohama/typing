58e0f8d00ba82ce27e9fa355bc3cc86ee1f66e17
yokohama <yuhei.yokohama@gmail.com>
2024-11-19 18:56:08 +0900

    ボタンデザイン

--- front/src/components/Login.tsx
+++ front/src/components/Login.tsx
@@ -48,13 +48,24 @@ export default function Login() {
 
   return (
     <button 
-      onClick={() => signIn('google')}
-      className="
-        px-4 py-2 
-        bg-blue-600 
-        text-white 
-        rounded-md 
-        hover:bg-blue-700
-    ">Sign in with Google</button>
+       onClick={() => signIn('google')}
+       className="
+         flex items-center justify-center
+         px-4 py-2 
+         bg-white 
+         text-gray-700 
+         border-2 
+	 border-gray-400 
+         rounded-md 
+         hover:bg-gray-100
+       "
+    >
+       <img 
+         src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" 
+         alt="Google logo" 
+         className="h-5 w-5 mr-2" 
+       />
+       Sign in
+    </button>
   );
 }
