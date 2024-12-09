24e45c1d9aaf6a448d50ed0706f2e69d21998486
yokohama <yuhei.yokohama@gmail.com>
2024-12-09 19:08:45 +0900

    fix invitation url

--- front/src/app/pair/page.tsx
+++ front/src/app/pair/page.tsx
@@ -47,7 +47,7 @@ export default function Page() {
 
   useEffect(() => {
     if (userInfo) {
-      setInvitationUrl(`${frontUrl}/invitation?inviteChildUserId=${userInfo?.id}`);
+      setInvitationUrl(`${frontUrl}/pair/invitation?inviteChildUserId=${userInfo?.id}`);
     }
   }, [userInfo]);
 
