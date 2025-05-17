902a3f2f9859f3ab545429e6cfa801004753f766
shishidochiyou <yesterday@aol.jp>
2025-05-18 00:47:06 +0900

    ランキングのリンクの追加

--- front/src/app/result/page.tsx
+++ front/src/app/result/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 import { Result } from '@/types/result';
 import { fetchData } from '@/lib/api';
@@ -27,7 +28,7 @@ export default function Page() {
     setSortedResults(
       results
         ?.filter((result) => result.created_at !== undefined)
-        .sort((a, b) => 
+        .sort((a, b) =>
           new Date(b.created_at!).getTime() - new Date(a.created_at!).getTime()
         )
     );
@@ -35,6 +36,15 @@ export default function Page() {
 
   return(
     <div className="overflow-x-auto p-4">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-xl font-semibold">レコード</h1>
+        <Link
+          href="/players"
+          className="bg-indigo-500 hover:bg-indigo-600 text-white px-3 py-1 rounded text-sm" >
+          ランキングを見る
+        </Link>
+      </div>
+
       {sortedResults ? (
         <ResultTable sortedResults={sortedResults} />
       ) : (
